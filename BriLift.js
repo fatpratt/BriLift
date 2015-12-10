67959041c171dcd254ed514104002d5d9f7fa535
Brian Pratt <brian.pratt@pearson.com>
2015-12-09 20:40:22 -0700

    work done

--- BriLift.js
+++ BriLift.js
@@ -18,42 +18,43 @@ function setSelFloor(floor) {
     };
 
     Simulator.prototype = {
-        setSelElevator: function(e) {this.selElevator}
-        setSelFloor: function(f) {this.selFloor}
+        setSelElevator: function(e) {this.selElevator = e;},
+        setSelFloor: function(f) {this.selFloor = f;},
+        startSim: function(){ console.log('start sim');}
     };
 
     $(document).ready(function() {
         console.log('ready');
-        $(startSimBtn).click(console.log('start'));
+        
+        $('#startSimBtn').click(function() {
 
-        var numElevators = $('#numElevs').val();    // todo: add error checking
-        var numFloors = $('#numFloors').val();
+            var numElevators = $('#numElevs').val();    // todo: add error checking
+            var numFloors = $('#numFloors').val();
 
-        simulator = new Simulator(numElevators, numFloors);
-        simulator.startSim();
+            simulator = new Simulator(numElevators, numFloors);
 
-        $('#elevBtns').html('');
-        $('#elevSels').html('')
-       
-        var makeBtnRow = function(label, num) {
-            // okay I stole this code from another project
-            for (var i = 0; i < num; i++) {
-                var element = document.createElement("input");
-                element.setAttribute("type", "button");
-                element.setAttribute("value", i + 1);
-                if (label == 'elevSels')
-                    element.setAttribute("onclick", "setSelElevator(" + i + ")");
-                if (label == 'elevBtns')
-                    element.setAttribute("onclick", "setSelFloor(" + i + ")");
+            $('#elevBtns').html('');
+            $('#elevSels').html('')
+           
+            var makeBtnRow = function(label, num) {
+                // okay I stole this code from another project
+                for (var i = 0; i < num; i++) {
+                    var element = document.createElement("input");
+                    element.setAttribute("type", "button");
+                    element.setAttribute("value", i + 1);
+                    if (label == 'elevSels')
+                        element.setAttribute("onclick", "setSelElevator(" + i + ")");
+                    if (label == 'elevBtns')
+                        element.setAttribute("onclick", "setSelFloor(" + i + ")");
 
-                var btnBarDiv = document.getElementById(label);
-                btnBarDiv.appendChild(element);
+                    var btnBarDiv = document.getElementById(label);
+                    btnBarDiv.appendChild(element);
+                }
             }
-        }
 
-        makeBtnRow('elevBtnBar', parseInt(numElevators, 10));
-        makeBtnRow('elevFloorBar', parseInt(numFloors, 10));
-
+            makeBtnRow('elevBtnBar', parseInt(numElevators, 10));
+            makeBtnRow('floorBtnBar', parseInt(numFloors, 10));
+        });
     });
 })();
 
