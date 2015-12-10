f67b1c01813ed1ad2c1141ebafbca19ef0445d25
Brian Pratt <brian.pratt@pearson.com>
2015-12-09 20:31:17 -0700

    work done

--- BriLift.js
+++ BriLift.js
@@ -1,23 +1,66 @@
 
+var simulator = null;       // global simulator
+
+function setSelElevator(elev) {
+    simulator.setSelElevator(elev);
+};
+
+function setSelFloor(floor) {
+    simulator.setSelFloor(floor);  
+};
 
 (function() {
     var Simulator = function(numElevators, numFloors) {
         this.numElevators = numElevators;
         this.numFloors = numFloors;
+        this.selFloor = 0;
+        this.selElevator = 0;
     };
 
     Simulator.prototype = {
-        test: function() {'log hello'}
+        setSelElevator: function(e) {this.selElevator}
+        setSelFloor: function(f) {this.selFloor}
     };
 
     $(document).ready(function() {
         console.log('ready');
         $(startSimBtn).click(console.log('start'));
 
-        var numElevators = $('#numElevs').val();
+        var numElevators = $('#numElevs').val();    // todo: add error checking
         var numFloors = $('#numFloors').val();
 
-        var simulator = new Simulator(numElevators, numFloors);
-        simulator.test();
+        simulator = new Simulator(numElevators, numFloors);
+        simulator.startSim();
+
+        $('#elevBtns').html('');
+        $('#elevSels').html('')
+       
+        var makeBtnRow = function(label, num) {
+            // okay I stole this code from another project
+            for (var i = 0; i < num; i++) {
+                var element = document.createElement("input");
+                element.setAttribute("type", "button");
+                element.setAttribute("value", i + 1);
+                if (label == 'elevSels')
+                    element.setAttribute("onclick", "setSelElevator(" + i + ")");
+                if (label == 'elevBtns')
+                    element.setAttribute("onclick", "setSelFloor(" + i + ")");
+
+                var btnBarDiv = document.getElementById(label);
+                btnBarDiv.appendChild(element);
+            }
+        }
+
+        makeBtnRow('elevBtnBar', parseInt(numElevators, 10));
+        makeBtnRow('elevFloorBar', parseInt(numFloors, 10));
+
     });
 })();
+
+// for automated testing later
+function autoTestHook() {
+    simulator = new Simulator(numElevators, numFloors);
+    simulator.startSim();  
+    setSelElevator(3);
+    ssetSelFloor(4);
+};
