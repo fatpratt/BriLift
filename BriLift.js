5e4442ee133b4c9da54d78ded0977ce5d768f3c6
Brian Pratt <brian.pratt@pearson.com>
2015-12-09 21:02:56 -0700

    work done

--- BriLift.js
+++ BriLift.js
@@ -10,17 +10,51 @@ function setSelFloor(floor) {
 };
 
 (function() {
+
+    var Elevator = function(elevNum) {
+        this.elevNum = elevNum;
+        this.curFloor = 0;
+        this.desiredFloor = 0;
+        console.log('elev create' + elevNum);
+    }
+
+    Elevator.prototype = {
+        advance: function() {console.log('advance');},
+        setDesiredFloor: function(f) {this.desiredFloor = f; console.log('desiredfloor')}
+    }
+
     var Simulator = function(numElevators, numFloors) {
         this.numElevators = numElevators;
         this.numFloors = numFloors;
         this.selFloor = 0;
         this.selElevator = 0;
+        this.elevatorList = [];
     };
 
     Simulator.prototype = {
         setSelElevator: function(e) {this.selElevator = e;},
-        setSelFloor: function(f) {this.selFloor = f;},
-        startSim: function(){ console.log('start sim');}
+        setSelFloor: function(f) {
+            this.selFloor = f; 
+            this.elevatorList[this.selElevator].setDesiredFloor(f)
+        },
+
+        startSim: function(){ 
+            console.log('start sim');
+            this.elevatorList = [];
+            for (var i = 0; i < this.numElevators; i++) {
+                this.elevatorList.push(new Elevator(i));
+            }
+
+            // --- main simulator loop ---
+            var self = this;
+            var simLoop = function() {      
+                for (var j = 0; j < self.elevatorList.length; j++) {
+                    self.elevatorList[j].advance();
+                }
+                setTimeout(simLoop, 3000);
+            }
+            setTimeout(simLoop, 3000);
+        }
     };
 
     $(document).ready(function() {
@@ -32,6 +66,7 @@ function setSelFloor(floor) {
             var numFloors = $('#numFloors').val();
 
             simulator = new Simulator(numElevators, numFloors);
+            simulator.startSim(); 
 
             $('#elevBtns').html('');
             $('#elevSels').html('')
